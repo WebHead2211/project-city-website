f03330b17e307d2e9357d74b22cf394c2da07714
Aayush Bakre <29601578+WebHead2211@users.noreply.github.com>
2024-07-09 13:12:03 +0530

    Footer static changes

--- src/components/header-components/Footer.jsx
+++ src/components/header-components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/Footer.css";
 
 export default function Footer() {
@@ -7,9 +7,7 @@ export default function Footer() {
     let scrollMax = document.body.scrollHeight - window.innerHeight;
     let currentScrollPos = window.scrollY;
     if (
-      (prevScrollpos > currentScrollPos ||
-      currentScrollPos >= scrollMax) 
-      &&
+      (prevScrollpos > currentScrollPos || currentScrollPos >= scrollMax) &&
       currentScrollPos > 0
     ) {
       document.querySelector("footer").className = "active";
@@ -19,9 +17,26 @@ export default function Footer() {
     prevScrollpos = currentScrollPos;
   });
 
+  // useEffect(() => {}, []);
+
   return (
-    <footer className="" style={{display: "flex", alignItems: "center"}}>
+    <footer
+      className=""
+      style={{ display: "flex", alignItems: "center", position: "relative" }}
+    >
       <h1>Made by Aayush Bakre</h1>
+      <div
+        className="temperature"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          position: "absolute",
+          right: "10px",
+        }}
+      >
+        <i class="fa-solid fa-sun"></i>
+        30 C
+      </div>
     </footer>
   );
 }
